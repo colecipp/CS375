815ad82e83fb24fb339ac6774f49cacdaff942e4
colecipp <42648284+colecipp@users.noreply.github.com>
2021-03-05 16:09:28 -0500

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    char const* const market-price-file = argv[1];
+    char const* const price-list-file = argv[1]; /* should check that argc > 1 */
+    FILE* file1 = fopen(market-price-file, "r");
+    FILE* file2 = fopen(price-list-file, "r"); /* should check the result */
+    char line[256];
+
+    while (fgets(line, sizeof(line), file1)) {
+        /* note that fgets don't strip the terminating \n, checking its
+           presence would allow to handle lines longer that sizeof(line) */
+        printf("%s", line);
+    }
+    /* may check feof here to make a difference between eof and io failure -- network
+       timeout for instance */
+
+    fclose(file1);
+
+    return 0;
+}
+// COMPUTEMAXPROFIT (I, W)
+//   maxProfit = 0;
+//   S = the empty set;
+//   M = the empty set;
+//   if the sum of the weights in I <= W
+//     return the profit of the items in I, and I;
+//   while more subsets to generate
+//     if the sum of the weights in S <= W
+//       if the profit of the items in S > maxProfit
+//         update maxProfit;
+//         copy S to M;
+//     generate the next subset S;
+// return maxProfit and M;
+    /* checking if a substring is in a string */
+// if(strstr(string, substring) != NULL) {
+//     /* ... */
+// }
---
+++ readme.txt
@@ -0,0 +1,2 @@
+C
+program1.c
