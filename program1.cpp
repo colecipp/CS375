02af811b0b582695dcb0834da1224df0ff77193c
colecipp <42648284+colecipp@users.noreply.github.com>
2021-03-07 19:12:47 -0500

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ifstream testFileM;
+    ifstream testFileP;
+    int maxProfit = 0;
+    set1<string, int> mPrice{};
+    set2<string, int> pPrice{};
+
+    for(int i = 1; i<argc;i++ ){
+        string arg = argv[i];
+        if (arg=="-m"){
+          string arg2 = argv[i+1];
+          testFileM.open(arg2.c_str());
+          if(testFileM.fail){
+            break;
+          }
+          else{
+            market_prices = maker(arg2.c_str());
+          }
+        }
+        if (arg=="-p"){
+          string arg2 = argv[i+1];
+          testFileP.open(arg2.c_str());
+          if(testFileP.fail){
+            break;
+          }
+        }
+    }
+    
+}
